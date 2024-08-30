21be1badb77c881f9ab8626f8869570c94b70cf1
k4karole <k4karole@gmail.com>
2024-08-30 06:32:50 +0100

    reorg. + last used currencies added

--- xchange/src/main/java/gi/Functions.java
+++ xchange/src/main/java/gi/Functions.java
@@ -8,52 +8,81 @@ import java.util.HashMap;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+
 public class Functions {
 
-    static JsonNode jsonNode;
-    static HashMap<String,String> mapJsonDetails;
+    static JsonNode jsonNodeLastUsedCurrencies;
+    static HashMap<String,String> mapJsonLastUsedCurrencies;
+    static String lastUsedCurrFrom;
+    static String lastUsedCurrTo;
+    
+    
+    static JsonNode jsonNodeApi;
+    static HashMap<String,String> mapJsonDetailsString;
     static HashMap<String,Integer> mapJsonDetailsInteger;
     static HashMap<String,HashMap<String,Double>> mapRates;
 
-    static File jsonFile = new File("./docs/learning/apiResponse.json");
+    static File jsonFileApi = new File("./docs/learning/apiResponse.json");
+    static File jsonFileLastUsedCurrencies = new File("./docs/lastUsedCurrencies.json");
     static ObjectMapper objectMapper = new ObjectMapper();
 
 
-    public static HashMap<String,String> getMapJsonDetailsString() {
+
+    static JsonNode generateJsonNode(File jsonFile, JsonNode jsonNode) {
         try {
             jsonNode = objectMapper.readTree(jsonFile);
-            mapJsonDetails = objectMapper.convertValue(jsonNode, HashMap.class);
-            
         } catch (IOException e) {System.out.println("ERROR: Read tree-file");}
         
-        return mapJsonDetails;
+        return jsonNode;
     }
 
 
-    public static HashMap<String,Integer> getMapJsonDetailsInteger() {
-        try {
-            jsonNode = objectMapper.readTree(jsonFile);
-            mapJsonDetailsInteger = objectMapper.convertValue(jsonNode, HashMap.class);
-            
-        } catch (IOException e) {System.out.println("ERROR: Read tree-file");}
-        
-        return mapJsonDetailsInteger;
+    /*
+     * LAST USED CURRENCIES
+     */
+    static HashMap<String,String> getMapLastUsedCurrencies() {
+        return objectMapper.convertValue(jsonNodeLastUsedCurrencies, HashMap.class);
     }
 
 
-    public static HashMap<String,HashMap<String,Double>> getMapRates() {
-        try {
-            jsonNode = objectMapper.readTree(jsonFile);
-            mapRates = objectMapper.convertValue(jsonNode, HashMap.class);
-            
-        } catch (IOException e) {System.out.println("ERROR: Read tree-file");}
-        
-        return mapRates;
+    static void getLastUsedCurrencies() {
+        jsonNodeLastUsedCurrencies = generateJsonNode(jsonFileLastUsedCurrencies, jsonNodeLastUsedCurrencies);
+        mapJsonLastUsedCurrencies = getMapLastUsedCurrencies();
+        lastUsedCurrFrom = mapJsonLastUsedCurrencies.get("from");
+        lastUsedCurrTo = mapJsonLastUsedCurrencies.get("to");
+    }
+
+
+
+
+    /*
+     * MAPS CREATION FROM API/JSON
+     */
+    static HashMap<String,String> getMapJsonDetailsString() {
+        return objectMapper.convertValue(jsonNodeApi, HashMap.class);
+    }
+
+
+    static HashMap<String,Integer> getMapJsonDetailsInteger() {
+        return objectMapper.convertValue(jsonNodeApi, HashMap.class);
+    }
+
+
+    static HashMap<String,HashMap<String,Double>> getMapRates() {
+        return objectMapper.convertValue(jsonNodeApi, HashMap.class);
+    }
+
+
+    public static void generateApiMaps() {
+        jsonNodeApi = generateJsonNode(jsonFileApi, jsonNodeApi);
+        mapJsonDetailsString = getMapJsonDetailsString();
+        mapJsonDetailsInteger = getMapJsonDetailsInteger();
+        mapRates = getMapRates();
     }
 
 
     public static boolean isResponseValid() {
-        String result = getMapJsonDetails().get("result");
+        String result = mapJsonDetailsString.get("result");
         if (result.equals("success")) {
             return true;
         }
@@ -64,18 +93,18 @@ public class Functions {
 
 
     public static String lastUpdateTimeUtc() {
-        String fullTime = getMapJsonDetails().get("time_last_update_utc");
+        String fullTime = mapJsonDetailsString.get("time_last_update_utc");
         return fullTime.substring(0, 25);
     }
 
 
     public static Integer nextUpdateTimeUnix() {
-        return getMapJsonDetailsInteger().get("time_next_update_unix");
+        return mapJsonDetailsInteger.get("time_next_update_unix");
     }
 
 
     public static Double getRate(String currencyCode) {
-        return getMapRates().get("rates").get(currencyCode);
+        return mapRates.get("rates").get(currencyCode);
     }
 
 
@@ -92,14 +121,15 @@ public class Functions {
     }
 
 
+
     public static void main(String[] args) {
 
-        // System.out.println(getMapJsonDetails());
-        // getMapRates();
-        // System.out.println(isResponseValid());
-        // System.out.println(lastUpdateTimeUtc());
-        // System.out.println(getRate("EUR"));
-        System.out.println(canGetNewApiRequest());
+        generateApiMaps();
+        getLastUsedCurrencies();
+
+        System.out.println(lastUsedCurrFrom);
+        System.out.println(getRate("HUF"));
+        System.out.println(lastUpdateTimeUtc());
         
     }
 
