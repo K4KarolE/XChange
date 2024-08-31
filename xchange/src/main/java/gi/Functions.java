46d0812529bd29708f77141cbed7a76d83ea0103
k4karole <k4karole@gmail.com>
2024-08-31 09:53:11 +0100

    api call if appropiate -> save response if vallid

--- xchange/src/main/java/gi/Functions.java
+++ xchange/src/main/java/gi/Functions.java
@@ -7,9 +7,16 @@ import java.util.HashMap;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 
 public class Functions {
+
+    static apiConnect apiCall = new apiConnect();
+    static JsonNode jsonNodeApiResponse;
+    static HashMap<String,String> mapJsonApiResponsString;
+    static HashMap<String,Integer> mapJsonApiResponsInteger;
+    static HashMap<String,HashMap<String,Double>> mapRatesApiRespons;
 
     static JsonNode jsonNodeLastUsedCurrencies;
     static HashMap<String,String> mapJsonLastUsedCurrencies;
@@ -17,12 +24,12 @@ public class Functions {
     static String lastUsedCurrTo;
     
     
-    static JsonNode jsonNodeApi;
+    static JsonNode jsonNodeJSON;
     static HashMap<String,String> mapJsonDetailsString;
     static HashMap<String,Integer> mapJsonDetailsInteger;
     static HashMap<String,HashMap<String,Double>> mapRates;
 
-    static File jsonFileApi = new File("./docs/learning/apiResponse.json");
+    static File jsonFileApi = new File("./docs/apiResponse.json");
     static File jsonFileLastUsedCurrencies = new File("./docs/lastUsedCurrencies.json");
     static ObjectMapper objectMapper = new ObjectMapper();
 
@@ -56,42 +63,31 @@ public class Functions {
 
 
     /*
-     * MAPS CREATION FROM API/JSON
+     * MAPS CREATION FROM SAVED JSON
      */
-    static HashMap<String,String> getMapJsonDetailsString() {
-        return objectMapper.convertValue(jsonNodeApi, HashMap.class);
+    static HashMap<String,String> getMapJsonDetailsString(JsonNode jsonNode) {
+        return objectMapper.convertValue(jsonNode, HashMap.class);
     }
 
 
-    static HashMap<String,Integer> getMapJsonDetailsInteger() {
-        return objectMapper.convertValue(jsonNodeApi, HashMap.class);
+    static HashMap<String,Integer> getMapJsonDetailsInteger(JsonNode jsonNode) {
+        return objectMapper.convertValue(jsonNode, HashMap.class);
     }
 
 
-    static HashMap<String,HashMap<String,Double>> getMapRates() {
-        return objectMapper.convertValue(jsonNodeApi, HashMap.class);
+    static HashMap<String,HashMap<String,Double>> getMapRates(JsonNode jsonNode) {
+        return objectMapper.convertValue(jsonNode, HashMap.class);
     }
 
 
-    public static void generateApiMaps() {
-        jsonNodeApi = generateJsonNode(jsonFileApi, jsonNodeApi);
-        mapJsonDetailsString = getMapJsonDetailsString();
-        mapJsonDetailsInteger = getMapJsonDetailsInteger();
-        mapRates = getMapRates();
+    public static void generateJsonMaps() {
+        jsonNodeJSON = generateJsonNode(jsonFileApi, jsonNodeJSON);
+        mapJsonDetailsString = getMapJsonDetailsString(jsonNodeJSON);
+        mapJsonDetailsInteger = getMapJsonDetailsInteger(jsonNodeJSON);
+        mapRates = getMapRates(jsonNodeJSON);
     }
 
-
-    public static boolean isResponseValid() {
-        String result = mapJsonDetailsString.get("result");
-        if (result.equals("success")) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
-
+    
     public static String lastUpdateTimeUtc() {
         String fullTime = mapJsonDetailsString.get("time_last_update_utc");
         return fullTime.substring(0, 25);
@@ -121,16 +117,49 @@ public class Functions {
     }
 
 
+    /*
+     *  API RESPONSE
+     */
+    public static void generateApiResponseMaps() {
+        mapJsonApiResponsString = getMapJsonDetailsString(jsonNodeApiResponse);
+        mapJsonApiResponsInteger = getMapJsonDetailsInteger(jsonNodeApiResponse);
+        mapRatesApiRespons = getMapRates(jsonNodeApiResponse);
+    }
+
+
+    public static boolean isResponseValid() {
+        String result = mapJsonApiResponsString.get("result");
+        if (result.equals("success")) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
 
     public static void main(String[] args) {
 
-        generateApiMaps();
-        getLastUsedCurrencies();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        generateJsonMaps();
 
-        System.out.println(lastUsedCurrFrom);
-        System.out.println(getRate("HUF"));
-        System.out.println(lastUpdateTimeUtc());
-        
+
+        if (canGetNewApiRequest()) {
+            jsonNodeApiResponse = apiCall.generateJsonNode();
+            
+            if (jsonNodeApiResponse != null) {
+                generateApiResponseMaps();
+            }
+
+            if (isResponseValid()) {
+                try {
+                    objectMapper.writeValue(jsonFileApi, jsonNodeApiResponse);
+                    generateJsonMaps();
+                }
+                catch (Exception e) {System.out.println("ERROR: Could not write API response to file.");}
+            }
+        }
     }
 
 }
---
+++ xchange/src/main/java/gi/apiConnect.java
@@ -0,0 +1,54 @@
+
+package gi;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+public class apiConnect {
+
+    static String URL = "https://open.er-api.com/v6/latest/USD"; 
+    static HttpClient client = HttpClient.newHttpClient();
+    static ObjectMapper objectMapper = new ObjectMapper();
+    static JsonNode jsonNode;
+
+
+    static String apiGet() {
+        
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(URL))
+                .GET()
+                .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            
+            objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+            
+            return response.body();
+        }
+        catch (Exception e) {
+            System.out.println("ERROR: API connect");
+            return "error";
+        }
+    }
+
+
+    public static JsonNode generateJsonNode() {
+        String apiResponse = apiGet();
+
+        if (!apiResponse.equals("error")) {
+            try {
+                jsonNode = objectMapper.readTree(apiResponse);
+                }
+            catch (IOException e) {System.out.println("ERROR: Read API string");}
+        }
+        return jsonNode;
+    }
+}
